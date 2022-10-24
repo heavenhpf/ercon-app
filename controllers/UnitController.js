1cea5832cbff71885b0dfccf3ccb5c242d2aa584
GRASSPHIC <tazki.amri@gmail.com>
2022-10-24 16:05:08 +0700

    Database unit revision

--- controllers/UnitController.js
+++ controllers/UnitController.js
@@ -10,15 +10,15 @@ app.get('/', userSession, async (req, res, next) => {
 })
 
 app.post('/', userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.addUnit(req.body))
+    response.sendResponse(res, await modules.addUnit(req.user.id, req.body))
 })
 
-app.put('/:id', userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.editUnit(Number(req.params.id), req.body))
+app.put('/:id_item', userSession, async (req, res, next) => {
+    response.sendResponse(res, await modules.editUnit(req.user.id, Number(req.params.id_item), req.body))
 })
 
-app.delete('/:id', userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.deleteUnit(Number(req.params.id)))
+app.delete('/:id_item', userSession, async (req, res, next) => {
+    response.sendResponse(res, await modules.deleteUnit(req.user.id, Number(req.params.id_item)))
 })
 
 module.exports = app
--- modules/unit.modules.js
+++ modules/unit.modules.js
@@ -24,9 +24,15 @@ class _unit {
         }
     }
 
-    addUnit = async (body) => {
+    addUnit = async (id_user, body) => {
         try {
+            body = {
+                id_user,
+                ...body
+            }
+
             const schema = Joi.object({
+                id_user: Joi.number().required(),
                 name: Joi.string().required()
             })
 
@@ -42,8 +48,27 @@ class _unit {
                 }
             }
 
+            const check = await prisma.s_company.findFirst({
+                where: {
+                    id_user: body.id_user,
+                    deleted_at: null
+                },
+                select: {
+                    id_company: true
+                }
+            })
+
+            if (!check) {
+                return {
+                    status: false,
+                    code: 404,
+                    error: "Data not found"
+                }
+            }
+
             const add = await prisma.ref_unit.create({
                 data: {
+                    id_company: check.id_company,
                     name: body.name,
                 }
             })
@@ -62,14 +87,17 @@ class _unit {
         }
     }
 
-    editUnit = async (id, body) => {
+    editUnit = async (id_user, id_unit, body) => {
         try {
             body = {
-                id,
+                id_user,
+                id_unit,
                 ...body
             }
+
             const schema = Joi.object({
-                id: Joi.number().required(),
+                id_user: Joi.number().required(),
+                id_unit: Joi.number().required(),
                 name: Joi.string()
             })
 
@@ -85,14 +113,25 @@ class _unit {
                 }
             }
 
-            const check = await prisma.ref_unit.findFirst({
+            const checkCompany = await prisma.s_company.findFirst({
                 where: {
-                    id_unit: body.id,
+                    id_user: body.id_user,
+                    deleted_at: null
+                },
+                select: {
+                    id_company: true
+                }
+            })
+
+            const checkUnit = await prisma.ref_unit.findFirst({
+                where: {
+                    id_unit: body.id_unit,
+                    id_company: checkCompany.id_company,
                     deleted_at: null
                 }
             })
 
-            if (!check) {
+            if (!(checkCompany && checkUnit)) {
                 return {
                     status: false,
                     code: 404,
@@ -102,7 +141,7 @@ class _unit {
 
             const edit = await prisma.ref_unit.update({
                 where: {
-                    id_unit: body.id
+                    id_unit: body.id_unit
                 },
                 data: {
                     name: body.name,
@@ -123,11 +162,19 @@ class _unit {
         }
     }
 
-    deleteUnit = async (id) => {
+    deleteUnit = async (id_user, id_unit) => {
         try {
-            const schema = Joi.number().required()
+            const body = {
+                id_user,
+                id_unit
+            }
 
-            const validation = schema.validate(id)
+            const schema = Joi.object({
+                id_unit: Joi.number().required(),
+                id_user: Joi.number().required()
+            })
+
+            const validation = schema.validate(body)
 
             if (validation.error) {
                 const errorDetails = validation.error.details.map(detail => detail.message)
@@ -139,14 +186,25 @@ class _unit {
                 }
             }
 
-            const check = await prisma.ref_unit.findFirst({
+            const checkCompany = await prisma.s_company.findFirst({
                 where: {
-                    id_unit: id,
+                    id_unit: body.id_unit,
+                    deleted_at: null
+                },
+                select: {
+                    id_company: true
+                }
+            })
+
+            const checkUnit = await prisma.ref_unit.findFirst({
+                where: {
+                    id_unit: body.id_unit,
+                    id_company: checkCompany.id_company,
                     deleted_at: null
                 }
             })
 
-            if (!check) {
+            if (!(checkCompany && checkUnit)) {
                 return {
                     status: false,
                     code: 404,
@@ -156,7 +214,7 @@ class _unit {
 
             const del = await prisma.ref_unit.update({
                 where: {
-                    id_unit: id
+                    id_unit: body.id_unit
                 },
                 data: {
                     deleted_at: new Date(Date.now())
