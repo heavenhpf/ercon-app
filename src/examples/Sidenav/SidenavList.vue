883a228dc3f57f4224828b56eead063216ca49c2
Heavenhpf <heavenhappynaputrafebriyono@gmail.com>
2022-12-05 14:35:23 +0700

    Completing Sidenav

--- src/examples/Sidenav/SidenavList.vue
+++ src/examples/Sidenav/SidenavList.vue
@@ -3,7 +3,7 @@
     
     <ul class="navbar-nav">
       <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-        <sidenav-item url="/dashboard" :class="getRoute() === 'default' ? 'active' : ''" navText="Dashboard">
+        <sidenav-item url="/dashboard" :class="getRoute() === 'default' ? 'active' : ''" navText="Dashboard" @click="MinSidenav()">
           <template v-slot:icon>
             <i class="fa fa-home mb-2 text-primary text-sm opacity-10"></i>
           </template>
@@ -16,7 +16,7 @@
       </li>
       <template v-if="this.g$user.role === 0 || this.g$user.role === 1 || this.g$user.role === 2">
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/tracking/tracking-saya" :class="getRoute() === 'tracking-saya' ? 'active' : ''"
+          <sidenav-item url="/tracking/tracking-saya" :class="getRoute() === 'tracking-saya' ? 'active' : ''" @click="MinSidenav()"
             navText="Tracking Saya">
             <template v-slot:icon>
               <i class="fa fa-paper-plane mb-2 text-success text-sm opacity-10"></i>
@@ -26,7 +26,7 @@
       </template>
       <template v-if="this.g$user.role === 0 || this.g$user.role === 1">
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/tracking/tracking-tier-bawah" :class="getRoute() === 'tracking-tier-bawah' ? 'active' : ''"
+          <sidenav-item url="/tracking/tracking-tier-bawah" :class="getRoute() === 'tracking-tier-bawah' ? 'active' : ''" @click="MinSidenav()"
             navText="Tracking Tier Bawah">
             <template v-slot:icon>
               <i class="fa fa-archive mb-2 text-success text-sm opacity-10"></i>
@@ -36,7 +36,7 @@
       </template>
       <template v-if="this.g$user.role === 1 || this.g$user.role === 2 || this.g$user.role === 3">
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/tracking/pesanan-masuk" :class="getRoute() === 'pesanan-masuk' ? 'active' : ''"
+          <sidenav-item url="/tracking/pesanan-masuk" :class="getRoute() === 'pesanan-masuk' ? 'active' : ''" @click="MinSidenav()"
             navText="Pesanan Masuk">
             <template v-slot:icon>
               <i class="fa fa-tasks  mb-2 text-success text-sm opacity-10"></i>
@@ -51,7 +51,7 @@
       </li>
       <template v-if="this.g$user.role === 1 || this.g$user.role === 2 || this.g$user.role === 3">
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/monitoring/gudang-saya" :class="getRoute() === 'gudang-saya' ? 'active' : ''"
+          <sidenav-item url="/monitoring/gudang-saya" :class="getRoute() === 'gudang-saya' ? 'active' : ''" @click="MinSidenav()"
             navText="Gudang Saya">
             <template v-slot:icon>
               <i class="ni ni-box-2 text-warning text-sm opacity-10"></i>
@@ -61,7 +61,7 @@
       </template>
       <template v-if="this.g$user.role === 0 || this.g$user.role === 1 || this.g$user.role === 2">
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/monitoring/monitoring-item" :class="getRoute() === 'monitoring-item' ? 'active' : ''"
+          <sidenav-item url="/monitoring/monitoring-item" :class="getRoute() === 'monitoring-item' ? 'active' : ''" @click="MinSidenav()"
             navText="Monitoring Item">
             <template v-slot:icon>
               <i class="ni ni-ui-04 text-warning text-sm opacity-10"></i>
@@ -76,7 +76,7 @@
           </h6>
         </li>
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/po/ajukan-po-1" :class="getRoute() === 'ajukan-po-1' ? 'active' : ''" navText="Ajukan PO">
+          <sidenav-item url="/po/ajukan-po-1" :class="getRoute() === 'ajukan-po-1' ? 'active' : ''" navText="Ajukan PO" @click="MinSidenav()">
             <template v-slot:icon>
               <i class="ni ni-tag text-info text-sm opacity-10"></i>
             </template>
@@ -89,7 +89,7 @@
         </h6>
       </li>
       <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-        <sidenav-item url="/akun/company-saya" :class="getRoute() === 'company-saya' ? 'active' : ''"
+        <sidenav-item url="/akun/company-saya" :class="getRoute() === 'company-saya' ? 'active' : ''" @click="MinSidenav()"
           navText="Perusahaan Saya">
           <template v-slot:icon>
             <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
@@ -98,7 +98,7 @@
       </li>
       <template v-if="this.g$user.role === 0">
         <li :class="`${this.$store.state.isRTL ? 'nav-item': 'nav-item d-flex justify-content-center'}`">
-          <sidenav-item url="/akun/company" :class="getRoute() === 'company' ? 'active' : ''" navText="List Perusahaan">
+          <sidenav-item url="/akun/company" :class="getRoute() === 'company' ? 'active' : ''" navText="List Perusahaan" @click="MinSidenav()">
             <template v-slot:icon>
               <i class="fa fa-users mb-1 text-dark text-sm opacity-10"></i>
             </template>
@@ -107,9 +107,6 @@
       </template>
       <!-- </template> -->
     </ul>
-  </div>
-  <div class="pt-3 mx-3 mt-3 sidenav-footer">
-    <sidenav-card :class="cardBg" textPrimary="Need Help?" textSecondary="Please check our docs" />
   </div>
 </template>
 
@@ -141,7 +138,10 @@ export default {
     getRoute() {
       const routeArr = this.$route.path.split("/");
       return routeArr[1];
-    }
+    },
+    MinSidenav() {
+      this.$store.state.isRTL = false;
+    },
   },
 
   computed: {
